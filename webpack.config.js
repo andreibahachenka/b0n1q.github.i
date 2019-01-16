e11b2ca221e8b39f356d51f522e73589ac59acf6
abahachenka <workwebstock@gmail.com>
2019-01-16 17:15:50 +0300

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: 'es2015.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: { presets: ["es2015", "es2016", "es2017"] }
+                    }
+                ]
+            }
+        ]
+    }
+};
